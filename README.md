cc512a814cf978c037749a5b580e4fbe8e2be0a5
HardRope <61661973+HardRope@users.noreply.github.com>
2022-04-23 15:36:56 +0300

    Add programm

---
+++ README.md
@@ -0,0 +1,16 @@
+# Bitly url shorterer
+This programm use the [Bitly service](https://bitly.com/) to make a shortlink orreturn counts of clicks after get the short link.
+
+## How to install
+To use the programm you need to create a '.env' file and add your Api-token here.
+Format of token in '.env' file looks like: "API-TOKEN = '2baa700828b03db\*\*\*\*\*\*\*\*\*\*fa90c64ad748b69'"
+You can get your Api-token after registration at [Bitly](https://bitly.com/). Link to create token: [link](https://app.bitly.com/settings/api/).
+
+Python3 should be already installed. Then use pip (or pip3, if there is a conflict with Python2) to install dependencies:
+
+'''
+pip install -r requirements.txt
+'''
+
+## Project Goals
+The code is written for educational purposes on online-course for web-developers dvmn.org.
---
+++ main.py
@@ -0,0 +1,81 @@
+from dotenv import load_dotenv
+import argparse
+import os
+import requests
+import json
+from urllib.parse import urlparse
+
+
+def shorten_link(url, headers):
+    payloads = {
+        'long_url': url,
+        "domain": "bit.ly",
+    }
+
+    response = requests.post(
+        'https://api-ssl.bitly.com/v4/shorten',
+        headers=headers,
+        json=payloads
+    )
+
+    response.raise_for_status()
+
+    return response.json()['link']
+
+
+def count_clicks(url, headers):
+    link = urlparse(url).netloc + urlparse(url).path
+
+    params = {"units": -1}
+
+    response = requests.get(
+        f'https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks/summary',
+        headers=headers,
+        params=params
+        )
+
+    response.raise_for_status()
+
+    return response.json()['total_clicks']
+
+
+def is_bitlink(url, headers):
+    link = urlparse(url).netloc + urlparse(url).path
+
+    response = requests.get(
+        f'https://api-ssl.bitly.com/v4/bitlinks/{link}',
+        headers=headers
+        )
+
+    return response.ok
+
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('link')
+    return parser
+
+
+def main():
+    load_dotenv()
+
+    parser = createParser()
+    user_link = parser.parse_args()
+
+    headers = {
+        'Authorization': 'Bearer {}'.format(os.environ['API-TOKEN'])
+    }
+
+    try:
+        requests.get(user_link.link)
+    except requests.exceptions.ConnectionError:
+        return 'Неверная ссылка'
+
+    if is_bitlink(user_link.link, headers):
+        return count_clicks(user_link.link, headers)
+    else:
+        return shorten_link(user_link.link, headers)
+
+
+if __name__ == '__main__':
+    print(main())
